b2ca353fa06f5fe1a1f45560bff98b46b7799b48
foysal <foysalf652@gmail.com>
2022-04-24 00:56:00 +0600

    searching section close

--- employee/urls.py
+++ employee/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("<int:id>/",views.employee_info_form, name= "employee_info_update"),
     path("view/<int:pk>/", views.employee_info_view, name ="views_info"),
     path("delete/<int:id>/", views.employee_info_delete, name="employee_delete"),
+    # path("pdf/", views.employee_form_info_pdf, name="employee_form_info_pdf"),
     
 ]
--- employee/views.py
+++ employee/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+
 from .forms import employeeForm
 from .models import Employee
+# import io
+# from reportlab.pdfgen import canvas  
+# from django.http import HttpResponse  
 
+
+# def employee_form_info_pdf(request):
+#     pass
 
 
 def employee_info_list(request):
+    if request.method == 'POST':
+        search = request.POST.get('search')
+        results = Employee.objects.filter(Q(full_name__icontains=search) | Q(emp_id__icontains=search) | Q(phone__icontains=search) | Q(email__icontains=search))
+        context =  { 'results': results, 'search': search}
+        return render(request, 'employee/search-result.html', context)
+  
     context = {'employee_info_list': Employee.objects.all()}
     return render (request, "employee/index.html", context)
 
@@ -47,3 +61,10 @@ def employee_info_delete(request, id):
     employee.delete()
     return redirect('/')
 
+
+# def search_result(request):
+#     if request.method == 'POST':
+#         search = request.POST.get('search')
+#         results = Employee.objects.filter(Q(full_name__icontains=search) | Q(emp_id__icontains=search))
+#         context =  { 'result': results, 'search': search}
+#         return render(request, 'employee/search-result.html', context)
